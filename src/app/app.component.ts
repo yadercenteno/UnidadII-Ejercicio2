8c5aa1b422c8ebf549d0bce235607de35998a100
Yader Centeno <yader_centeno@yahoo.com>
2020-08-24 16:58:33 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UnidadII-Ejercicio2';
+  titulo = 'Bienvenidos a la Validación de Formularios con Angular';
+  angForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(): void {
+    const regUrl = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+
+    this.angForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.maxLength(5)]],
+      apellido: ['', Validators.required],
+      edad: ['', [Validators.min(0), Validators.max(100)]],
+      sitioWeb: ['', [Validators.pattern(regUrl)]]
+    });
+
+    this.angForm.controls['nombre'].valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.angForm.valid) {
+      console.log(this.angForm.value);
+    } else {
+      alert('ERROR: El formulario es INVALIDO');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms'; // <---
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
